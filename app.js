0440902942cc3bba00412fc33ad95f3ea2d0fec1
rongjunming <rjm96@foxmail.com>
2021-12-31 10:11:22 +0800

    ✨ feat(新增年度总结文件):

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { debug } = require('console')
 var fs = require('fs')
 const shell = require('shelljs')
 
@@ -40,55 +41,106 @@ function getDate() {
 function addGame() {
   const game = {
     time: getDate(),
-    winner: ['yan', 'bing'],
-    loser: ['xu', 'ming'],
-    score: [10, 5]
+    winner: ['ming', 'bing'],
+    loser: ['xu', 'yu'],
+    score: [10, 4]
   }
   readFile(game)
 }
-addGame()
+// addGame()
 
-function rank() {
-  const players = ['bing', 'yan', 'yu', 'xu', 'ming', 'hanyu']
-  fs.readFile(filePath, function (err, data) {
+function rank(data) {
+  const players = ['bing', 'yan', 'yu', 'xu', 'ming']
+  let gameArr = data
+  if (!gameArr) {
+    try {
+      const data = fs.readFileSync(filePath)
+      gameArr = JSON.parse(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  const rankArr = []
+  // 统计每位选手的大比分,小比分,场次,组队最多/最少次的队友
+  players.forEach((p) => {
+    const pData = {
+      name: '',
+      big: 0,
+      small: 0,
+      num: 0,
+      score: 0,
+      xu: [0, 0],
+      yu: [0, 0],
+      yan: [0, 0],
+      bing: [0, 0],
+      ming: [0, 0],
+      averagingGoal: 0
+    }
+    gameArr.forEach((game) => {
+      const person = [...game.winner, ...game.loser]
+      if (person.includes(p)) {
+        pData.num += 1
+        // 统计当前选手队友出现的次数
+        const { winner, loser } = game
+        if (winner.includes(p)) {
+          const key = winner[0] === p ? winner[1] : winner[0]
+          // console.log(key, pData[key])
+          // debug(pData[key][0])
+          if (pData[key]) pData[key][0]++
+        }
+        if (loser.includes(p)) {
+          const key = loser[0] === p ? loser[1] : loser[0]
+          // console.log(key, pData[key])
+          if (pData[key]) pData[key][1]++
+        }
+      }
+      if (game.winner.includes(p)) {
+        pData.big += 1
+        pData.small += game.score[0]
+      }
+      if (game.loser.includes(p)) {
+        pData.small += game.score[1]
+      }
+    })
+    const score = pData.num > 0 ? (pData.big * 5 + pData.small) / pData.num : 0
+    pData.score = score.toFixed(2)
+    pData.name = p
+    pData.averagingGoal = (pData.small / pData.num).toFixed(2)
+    rankArr.push(pData)
+  })
+  rankArr.sort((a, b) => b.score - a.score)
+  console.log(
+    rankArr.map((item) => ({
+      name: item.name,
+      big: item.big,
+      small: item.small,
+      num: item.num,
+      score: item.score
+    }))
+  )
+  fs.writeFile('./score/summary.json', JSON.stringify(rankArr), function (err) {
     if (err) {
       console.log(err)
     } else {
-      const gameArr = JSON.parse(data)
-      const rankArr = []
-      // 统计每位选手的大比分,小比分,场次
-      players.forEach((p) => {
-        const pData = {
-          name: '',
-          big: 0,
-          small: 0,
-          num: 0,
-          score: 0
-          // averageScore: 0
-        }
-        gameArr.forEach((game) => {
-          const person = [...game.winner, ...game.loser]
-          if (person.includes(p)) {
-            pData.num += 1
-          }
-          if (game.winner.includes(p)) {
-            pData.big += 1
-            pData.small += game.score[0]
-          }
-          if (game.loser.includes(p)) {
-            pData.small += game.score[1]
-          }
-        })
-        const score = pData.num > 0 ? (pData.big * 5 + pData.small) / pData.num : 0
-        pData.score = score.toFixed(2)
-        pData.name = p
-        // pData.averageScore = Math.round(pData.small / pData.num)
-        rankArr.push(pData)
-      })
-      rankArr.sort((a, b) => b.score - a.score)
-      console.log(rankArr)
+      console.log('总数据写入成功！')
     }
   })
 }
-
 // rank()
+
+function summary() {
+  const pathArr = ['./score/score-s1.json', './score/score-s2.json', './score/score-s3.json', './score/score-s4.json']
+  let gameTotal = []
+  try {
+    pathArr.forEach((path) => {
+      const data = fs.readFileSync(path, 'utf-8')
+      const gameArr = JSON.parse(data)
+      gameTotal = gameTotal.concat(gameArr)
+    })
+    console.log('总局数', gameTotal.length)
+    rank(gameTotal)
+  } catch (error) {
+    console.error(error)
+  }
+}
+summary()
