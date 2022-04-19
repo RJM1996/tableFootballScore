f23a01cf9ba0562d1c0ff902d84bee301a498782
rongjunming <rjm96@foxmail.com>
2022-04-19 21:55:58 +0800

    S9 update

--- app.js
+++ app.js
@@ -43,9 +43,9 @@ function getDate () {
 function addGame () {
   const game = {
     time: getDate(),
-    winner: ['yu', 'bing'],
-    loser: ['yan', 'ming'],
-    score: [10, 5],
+    winner: ['ming', 'yan'],
+    loser: ['yu', 'bing'],
+    score: [10, 2],
   }
   readFile(game)
 }
